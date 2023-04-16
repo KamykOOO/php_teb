551d244a226ab3ef03df685796684490bbfbae5d
KamykOOO <4m4dik@gmail.com>
2023-04-16 16:43:31 +0200

    Add error message

--- index.php
+++ index.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace App;
 
+require_once('./exception/AppException.php');
 include_once('./src/Controller.php');
 include_once('./src/utils/debug.php');
 require_once('./config/config.php');
 
-Controller::initConfiguration($configuration);
-$controller = new Controller($_GET, $_POST);
-$controller->run();
+try {
+    Controller::initConfiguration($configuration);
+    $controller = new Controller($_GET, $_POST);
+    $controller->run();
+} catch (AppException $e) {
+    echo "<h1>Wystąpił błąd w aplikacji</h1>";
+    echo '<h3>' . $e->getMessage() . '<h3>';
+}
